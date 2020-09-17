924e05417e4238da631710b00ea41c2e87e0a953
Carly Bouts <cbouts@g.clemson.edu>
2020-09-17 13:46:00 -0400

    make folder

--- tmdb_request.py
+++ tmdb_request.py
@@ -2,13 +2,21 @@
 import urllib.request
 import json
 import sys
+import os
 
 api_key = sys.argv[1]
 
-response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/latest?api_key=' + api_key)
+if not os.path.exists('json_files'):
+	os.mkdir('json_files')
+
+# response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/latest?api_key=' + api_key)
+# json_response = json.load(response)
+# movie_count = int(json_response['id'])
+# print (movie_count)
+
+# movie_start = movie_count-5
+movie_id = '550'
+# how to do a for loop in order to get info on last 5 movies
+response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/' + movie_id +'?api_key=' + api_key)
 json_response = json.load(response)
-movie_count = int(json_response['id'])
-print (movie_count)
-
-
-# should in
+print(json_response)
