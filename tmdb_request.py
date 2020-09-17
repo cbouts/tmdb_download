21c4c60322365012b3f777e83af427a0b6c35a2b
Carly Bouts <cbouts@g.clemson.edu>
2020-09-17 13:33:36 -0400

    try doing a request

--- tmdb_request.py
+++ tmdb_request.py
@@ -1,1 +1,7 @@
-print("hello")
+# print("hello")
+import urllib.request
+import json
+
+response = urllib.request.urlopen('https://api.themoviedb.org/3/movie/550?api_key=a2e0b327cacf1f08847bb4fea510af55')
+json_response = json.load(response)
+print (json_response)
